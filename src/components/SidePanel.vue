f5a637758ab7e3e790d10364c5e77e18b1bd7fd0
Imani Paul <imanipaul@imanis-mbp.lan>
2020-10-12 20:03:59 -0400

    Add side panel component

---
+++ src/components/SidePanel.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="side-panel">
+    <img src="https://picsum.photos/150" />
+    <h1>Welcome</h1>
+    <h2>View ToDos Below:</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SidePanel",
+};
+</script>
+
+<style lang="scss" scoped>
+.side-panel {
+  height: 100vh;
+  width: 100px;
+  background-color: lightblue;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 3em;
+}
+</style>
