b3252c5332c713176979227c1886075e1479811e
Imani Paul <imani.paul1@gmail.com>
2020-10-12 20:10:27 -0400

    Add side panel component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SidePanel />
+  <div class="home">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import SidePanel from "./components/SidePanel.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    SidePanel,
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+body {
+  margin: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  // margin-top: 60px;
+  display: flex;
+  // width: 100vw;
+
+  .home {
+    flex: 3;
+    .hello {
+      border: 5px solid red;
+    }
+  }
+
+  // .side-panel {
+  //   flex: 1;
+  // }
 }
 </style>
